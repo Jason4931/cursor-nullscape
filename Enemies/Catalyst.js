35d6a5903435147a1922fd28217a7a97350935ea
Jason4931 <emmanuel.jason2008@gmail.com>
2026-01-18 00:25:23 +0700

    f u r t h e r

--- Enemies/Catalyst.js
+++ Enemies/Catalyst.js
@@ -37,7 +37,7 @@ export function setup(host) {
 
   /* ---------------- HELPERS ---------------- */
 
-  function randNearCursor(r = 500) {
+  function randNearCursor(r = 750) {
     const a = Math.random() * Math.PI * 2;
     const d = r * 0.75 + Math.random() * r * 0.25;
     return {
