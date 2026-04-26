d448d64e54e09d8c8f8626db68e2754c99bb8147
Jason4931 <emmanuel.jason2008@gmail.com>
2026-04-26 19:06:49 +0700

    intro timeout

--- main.js
+++ main.js
@@ -3193,6 +3193,7 @@ const unlock = () => {
 };
 window.addEventListener('click', unlock);
 document.getElementById("intro-screen").addEventListener('click', unlock);
+setTimeout(unlock, 10000);
 
 setInterval(() => {
   for (const [key, p] of patternsState) {
